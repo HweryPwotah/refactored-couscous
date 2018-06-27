a9f9d76931e7faf0751be090fd2339d49bb3c0eb
catalyst31 <adamcholid311@gmail.com>
2018-06-27 16:52:47 +0700

    implemented fragments, updated navigation drawer icons

---
+++ app/src/main/java/com/solution/ahe/walletdirector/RecordFragment.java
@@ -0,0 +1,28 @@
+package com.solution.ahe.walletdirector;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecordFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Records");
+    }
+}
